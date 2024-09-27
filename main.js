f32d5768a02a4d07dd597e596be88a1f03ea6d82
Bernat00 <szabobernat06@gmail.com>
2024-09-27 09:45:42 +0200

    aaaaa nem ertem a neveket

---
+++ main.js
@@ -0,0 +1,65 @@
+const array = [
+    {
+        firstname1: 'Géza',
+        firstname2: 'Ferenc',
+        lastname: 'Kocsis'
+    },
+    {
+        firstname1: 'Mária',
+        firstname2: 'Júlia',
+        lastname: 'Horváth'
+    },
+    {
+        firstname1: 'Ferenc',
+        lastname: 'Balogh'
+    },
+    {
+        firstname1: 'Gábor',
+        firstname2: 'Attila',
+        lastname: 'Horváth'
+    },
+]
+
+const table = document.createElement('table');
+const tableHeader = document.createElement('thead');
+const tableBody = document.createElement('tbody');
+const tableHeaderRow = document.createElement('tr');
+const tableHeaderFhirstName = document.createElement('th');
+const tableHeaderLastName = document.createElement('th');
+
+table.appendChild(tableHeader);
+table.appendChild(tableBody);
+tableHeader.appendChild(tableHeaderRow);
+tableHeaderRow.appendChild(tableHeaderFhirstName);
+tableHeaderRow.appendChild(tableHeaderLastName);
+
+tableHeaderFhirstName.innerHTML = 'Keresztnév';
+tableHeaderLastName.innerHTML = 'Vezetékév';
+tableHeaderFhirstName.colSpan = 2;
+
+for(let line of array){
+    const row = document.createElement('tr');
+    tableBody.appendChild(row);
+
+    const ln = document.createElement('td');
+    ln.innerHTML = line.lastname;
+    row.appendChild(ln);    
+
+    const fn1 = document.createElement('td');
+    fn1.innerHTML = line.firstname1;
+    row.appendChild(fn1);
+
+
+    if(line.firstname2){
+        const fn2 = document.createElement('td');
+        fn2.innerHTML = line.firstname2;
+        row.appendChild(fn2);
+    }
+    else {
+        ln.colSpan = 2;
+    }
+
+};
+
+document.body.appendChild(table);
+
